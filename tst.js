66339e61f48501b26edea3993cd159217a39c829
Sergey <userfoenix@gmail.com>
2016-06-02 18:04:47 +0300

    Create tst.js

---
+++ tst.js
@@ -0,0 +1,70 @@
+/**
+ * Created by SERG on 02.06.2016.
+ */
+var oMsg = {
+	corr: 'string', // guid or guid@group
+	text: 'string',
+	attach: 'object', // atatch object
+	atime: 'string', // 64 bitstring
+	flags: 'number' // bitmask
+};
+
+var cmd = [
+	{cmd:'msg.send',    payload: {corr:'string', text:'string'}},
+	{cmd:'msg.read',    payload: {corr:'string', atime:'64 bitstring'}},
+	{cmd:'msg.roster',  payload: {modtime:'[number]'}},
+	{cmd:'msg.history', payload: {corr:'string', modtime:'[number]'}},
+	{cmd:'msg.typing',  payload: {corr:'string'}}
+];
+
+var notification = [
+	{
+		cmd:'msg.roster', data: [{
+			id: 'string', // guid or group_id
+			type: 'number', // user,group
+			modtime: 'number' // last modification time for sort
+		}]
+	},
+	{
+		cmd:'msg.inbox', data: [oMsg]
+	},
+	{
+		cmd:'msg.history', data: [oMsg]
+	},
+	{
+		cmd:'msg.contact', data: [
+		{
+			guid: 'number', // user guid
+			name: 'string', // full name
+			pht: 'string', // Or binary, depends of implementation
+			gender: 'number',
+			online: 'number', // 0-offline, 1-invisible, 2-online
+			status: 'string' // additional user status
+		}
+	]},
+	{
+		cmd:'status', data: [
+		{ // only online or status also possible
+			guid: 'number', // user guid
+			online: 'number', // 0-offline, 1-invisible, 2-online
+			status: 'string' // additional user status
+		}
+	]},
+	{
+		cmd:'group', data: [
+		{
+			id: 'number', // Glocbal group ID
+			type: 'number', // normal,public,etc.. - default 0
+			name: 'string', // group name
+			avatar: 'string', // path to avatar ?????????
+			members: [{
+				guid: 'number', // member guid
+				status: 'status' // 0-pending, 1-approved, 2-deleted
+			}]
+		}
+	]}
+];
+
+// Нужно ли нам передавать отдельно группы и контакты
+// Разичачать гуид и ид группы или единый
+
